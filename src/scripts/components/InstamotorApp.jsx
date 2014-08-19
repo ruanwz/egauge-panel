afcb8339f72494a0cd84fc72676be54e2b025cc5
David Ruan <ruanwz@gmail.com>
2014-08-20 01:34:40 +0800

    add button to switch steps

--- src/scripts/components/InstamotorApp.jsx
+++ src/scripts/components/InstamotorApp.jsx
@@ -3,7 +3,6 @@
  */
 
 'use strict';
-
 var React = require('react/addons');
 var ReactTransitionGroup = React.addons.TransitionGroup;
 // Export React so the devtools can find it
@@ -16,14 +15,40 @@ require('../../styles/reset.css');
 require('../../styles/main.css');
 
 var imageURL = '../../images/yeoman.png';
-
+var _ = require('underscore');
+var steps = ["Step 1", "Step 2", "Step 3", "Step 4"];
 var InstamotorApp = React.createClass({
+
+  getInitialState: function() {
+    return {
+      current_step_name: 'Step 1'
+    };
+  },
+  previous_step: function(){
+    var current = this.state.current_step_name;
+    var position = _.indexOf(steps, current);
+
+    if (position > 0) {
+      this.setState({current_step_name: steps[position-1]});
+    }
+  },
+  next_step: function(){
+    var current = this.state.current_step_name;
+    var position = _.indexOf(steps, current);
+
+    if (position < steps.length - 1) {
+      this.setState({current_step_name: steps[position+1]});
+    }
+  },
   render: function() {
     return (
       <div className='main'>
         <ReactTransitionGroup transitionName="fade">
-          <img src={imageURL} />
-          <SimpleSteps current_step_name="Step 4"/>
+          <img src={imageURL} onClick={this.next_step}/>
+          <button type="button" className="btn btn-primary" onClick={this.previous_step}>Back</button>
+          <button type="button" className="btn btn-primary" onClick={this.next_step}>Next</button>
+
+          <SimpleSteps current_step_name={this.state.current_step_name}/>
 
         </ReactTransitionGroup>
       </div>
--- src/scripts/components/SimpleSteps.cjsx
+++ src/scripts/components/SimpleSteps.cjsx
@@ -22,9 +22,9 @@ StepComponent = CR.React.createClass
 SimpleSteps = CR.React.createClass
   render: ->
     <div className="row smpl-step" >
-      <StepComponent current_step_name={this.props.current_step_name} step_name="Step 1" icon_name="user" step_info="Registe User via control panel."/>
-      <StepComponent current_step_name={this.props.current_step_name} step_status="active" step_name="Step 2" icon_name="dollar" step_info="Process Payment and fill out all required fields."/>
-      <StepComponent current_step_name={this.props.current_step_name} step_status="disabled" step_name="Step 3" icon_name="repeat" step_info="Confirm Data entered in step 2"/>
-      <StepComponent current_step_name={this.props.current_step_name} step_status="disabled" step_name="Step 4" icon_name="download" step_info="Download product after receiving confirmation email"/>
+      <StepComponent current_step_name={this.props.current_step_name} step_name="Step 1" icon_name="user" step_info="User Authentication"/>
+      <StepComponent current_step_name={this.props.current_step_name} step_status="active" step_name="Step 2" icon_name="dollar" step_info="Choose Three Time Slots"/>
+      <StepComponent current_step_name={this.props.current_step_name} step_status="disabled" step_name="Step 3" icon_name="repeat" step_info="This Car is Pending Instamotor Mechanical Guarantee"/>
+      <StepComponent current_step_name={this.props.current_step_name} step_status="disabled" step_name="Step 4" icon_name="download" step_info="Email has been Sent"/>
     </div>
 module.exports = SimpleSteps
